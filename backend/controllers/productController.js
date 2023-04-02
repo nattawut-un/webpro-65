b92be3477cc6c0ae496c9e55bb85245d6bf467d1
Nattawut Un <baszaa1234@gmail.com>
2023-04-02 13:23:54 +0700

    product try catch

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,21 @@
 import { getProducts, getProductById } from '../models/productModel.js'
 
 export const showProducts = async (req, res, next) => {
-  const results = await getProducts()
-  res.json(results)
+  try {
+    const results = await getProducts()
+    res.json(results)
+  } catch (err) {
+    console.log(err)
+    next(err)
+  }
 }
 
 export const showProductById = async (req, res, next) => {
-  const results = await getProductById(req.params.id)
-  res.json(results)
+  try {
+    const results = await getProductById(req.params.id)
+    res.json(results)
+  } catch (err) {
+    console.log(err)
+    next(err)
+  }
 }
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,11 +1,19 @@
 import db from '../config/database.js'
 
 export const getProducts = async () => {
-  const [rows, fields] = await db.query('SELECT * FROM products')
-  return rows
+  try {
+    const [rows, fields] = await db.query('SELECT * FROM products')
+    return rows
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export const getProductById = async (id) => {
-  const [rows,fields] = await db.query("SELECT * FROM products WHERE id = ?", [id])
-  return rows[0]
+  try {
+    const [rows, fields] = await db.query("SELECT * FROM products WHERE id = ?", [id])
+    return rows[0]
+  } catch (err) {
+    console.log(err)
+  }
 }
