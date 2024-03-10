ee652d58aacc8db99359153ef49267c86d572f9f
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-03-10 21:53:47 +0700

    fix vuelidate in LoginPage

--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -12,7 +12,7 @@ import { required, minLength } from '@vuelidate/validators'
 export default {
   data() {
     return {
-      $v: useVuelidate(),
+      v$: useVuelidate(),
       store,
       username: '',
       password: '',
@@ -58,13 +58,13 @@ export default {
     <form>
       <div class="my-4">
         <label>ชื่อผู้ใช้:</label><br>
-        <input type="text" v-model="$v.username.$model" placeholder="Username" class="border-2 rounded-full mt-2 px-4 text-xl">
-        <label class="text-red-500 ml-2" v-if="$v.username.minLength.$invalid">ต้องยาวกว่า 5 ตัว</label>
+        <input type="text" v-model="v$.username.$model" placeholder="Username" class="border-2 rounded-full mt-2 px-4 text-xl">
+        <label class="text-red-500 ml-2" v-if="v$.username.minLength.$invalid">ต้องยาวกว่า 5 ตัว</label>
       </div>
       <div class="my-4">
         <label>รหัสผ่าน:</label><br>
-        <input type="password" v-model="$v.password.$model" placeholder="Password" class="border-2 rounded-full mt-2 px-4 text-xl">
-        <label class="text-red-500 ml-2" v-if="$v.password.minLength.$invalid">ต้องยาวกว่า 6 ตัว</label>
+        <input type="password" v-model="v$.password.$model" placeholder="Password" class="border-2 rounded-full mt-2 px-4 text-xl">
+        <label class="text-red-500 ml-2" v-if="v$.password.minLength.$invalid">ต้องยาวกว่า 6 ตัว</label>
       </div>
     </form><br><hr><br><br>
     <button @click="login()" class="bg-primary text-white font-bold px-6 py-2 rounded-full text-2xl">ลงชื่อเข้าใช้</button>
