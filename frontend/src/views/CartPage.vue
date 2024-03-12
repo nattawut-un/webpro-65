ca5d731e2a168a058e64e2ffee17b87d9a96c91e
Nattawut Un. <88489666+nattawut-un@users.noreply.github.com>
2024-03-13 00:26:44 +0700

    fix cart page

--- frontend/src/views/CartPage.vue
+++ frontend/src/views/CartPage.vue
@@ -57,7 +57,7 @@ export default {
         </div>
         <div class="w-1/3 text-end">
           <h1 class="text-6xl font-bold mb-2 font-chonburi">{{ item.price*item.quantity }}.-</h1>
-          <button class="bg-red-500 text-white rounded-full px-4 py-2" @click="removeFromCart(this.store.cart.indexOf(item))" :disabled="store.cart.length == 0">
+          <button class="bg-red-500 text-white rounded-full px-4 py-2" @click="removeFromCart(store.cart.indexOf(item))" :disabled="store.cart.length == 0">
             ลบ
           </button>
         </div>
